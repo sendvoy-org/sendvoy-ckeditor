02c52dc949c055f1ee2bf3f8341ca58a5c2d049e
Owoh Chidubem <chidubempaul91@gmail.com>
2023-10-06 07:28:44 +0100

    First Commit

---
+++ build/editor-util.d.ts
@@ -0,0 +1,9 @@
+export declare const REDUCED_MATERIAL_COLORS: {
+    label: string;
+    color: string;
+}[];
+export declare const EMOJIS_ARRAY: {
+    character: string;
+    title: string;
+}[];
+export declare function SpecialCharactersEmoji(editor: any): void;
---
+++ src/ckeditor.ts
@@ -0,0 +1,205 @@
+/**
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+// The editor creator to use.
+import { DecoupledEditor as DecoupledEditorBase } from "@ckeditor/ckeditor5-editor-decoupled";
+
+import { Alignment } from "@ckeditor/ckeditor5-alignment";
+import { Autoformat } from "@ckeditor/ckeditor5-autoformat";
+import { Autosave } from "@ckeditor/ckeditor5-autosave";
+import {
+  Bold,
+  Italic,
+  Strikethrough,
+  Subscript,
+  Superscript,
+  Underline,
+  Code,
+} from "@ckeditor/ckeditor5-basic-styles";
+import { BlockQuote } from "@ckeditor/ckeditor5-block-quote";
+import { CloudServices } from "@ckeditor/ckeditor5-cloud-services";
+import { Essentials } from "@ckeditor/ckeditor5-essentials";
+import { FindAndReplace } from "@ckeditor/ckeditor5-find-and-replace";
+import {
+  FontBackgroundColor,
+  FontColor,
+  FontFamily,
+  FontSize,
+} from "@ckeditor/ckeditor5-font";
+import { Heading } from "@ckeditor/ckeditor5-heading";
+import { Highlight } from "@ckeditor/ckeditor5-highlight";
+import { HorizontalLine } from "@ckeditor/ckeditor5-horizontal-line";
+import { GeneralHtmlSupport } from "@ckeditor/ckeditor5-html-support";
+import {
+  AutoImage,
+  Image,
+  ImageCaption,
+  ImageInsert,
+  ImageResize,
+  ImageStyle,
+  ImageToolbar,
+  ImageUpload,
+} from "@ckeditor/ckeditor5-image";
+import { Indent, IndentBlock } from "@ckeditor/ckeditor5-indent";
+import { AutoLink, Link, LinkImage } from "@ckeditor/ckeditor5-link";
+import { List, ListProperties, TodoList } from "@ckeditor/ckeditor5-list";
+import { MediaEmbed, MediaEmbedToolbar } from "@ckeditor/ckeditor5-media-embed";
+import { PageBreak } from "@ckeditor/ckeditor5-page-break";
+import { Paragraph } from "@ckeditor/ckeditor5-paragraph";
+import { PasteFromOffice } from "@ckeditor/ckeditor5-paste-from-office";
+import { RemoveFormat } from "@ckeditor/ckeditor5-remove-format";
+import { SelectAll } from "@ckeditor/ckeditor5-select-all";
+import {
+  SpecialCharacters,
+  SpecialCharactersArrows,
+  SpecialCharactersCurrency,
+  SpecialCharactersEssentials,
+  SpecialCharactersLatin,
+  SpecialCharactersMathematical,
+  SpecialCharactersText,
+} from "@ckeditor/ckeditor5-special-characters";
+import { Style } from "@ckeditor/ckeditor5-style";
+import {
+  Table,
+  TableCaption,
+  TableCellProperties,
+  TableColumnResize,
+  TableProperties,
+  TableToolbar,
+} from "@ckeditor/ckeditor5-table";
+import { TextTransformation } from "@ckeditor/ckeditor5-typing";
+import { Base64UploadAdapter } from "@ckeditor/ckeditor5-upload";
+import { WordCount } from "@ckeditor/ckeditor5-word-count";
+import { SpecialCharactersEmoji } from "./editor-util";
+
+export default class DecoupledEditor extends DecoupledEditorBase {
+  public static override builtinPlugins = [
+    Alignment,
+    AutoImage,
+    AutoLink,
+    Autoformat,
+    Autosave,
+    Base64UploadAdapter,
+    BlockQuote,
+    Bold,
+    CloudServices,
+    Essentials,
+    FindAndReplace,
+    FontBackgroundColor,
+    FontColor,
+    FontFamily,
+    FontSize,
+    GeneralHtmlSupport,
+    Heading,
+    Highlight,
+    HorizontalLine,
+    Image,
+    ImageCaption,
+    ImageInsert,
+    ImageResize,
+    ImageStyle,
+    ImageToolbar,
+    ImageUpload,
+    Indent,
+    IndentBlock,
+    Italic,
+    Link,
+    LinkImage,
+    List,
+    ListProperties,
+    MediaEmbed,
+    MediaEmbedToolbar,
+    PageBreak,
+    Paragraph,
+    PasteFromOffice,
+    RemoveFormat,
+    SelectAll,
+    SpecialCharacters,
+    SpecialCharactersArrows,
+    SpecialCharactersCurrency,
+    SpecialCharactersEssentials,
+    SpecialCharactersLatin,
+    SpecialCharactersMathematical,
+    SpecialCharactersText,
+    SpecialCharactersEmoji,
+    Strikethrough,
+    Style,
+    Subscript,
+    Superscript,
+    Table,
+    TableCaption,
+    TableCellProperties,
+    TableColumnResize,
+    TableProperties,
+    TableToolbar,
+    TextTransformation,
+    TodoList,
+    Underline,
+    Code,
+    WordCount,
+  ];
+
+  public static override defaultConfig = {
+    toolbar: {
+      items: [
+        "undo",
+        "redo",
+        "|",
+        "heading",
+        "|",
+        "fontSize",
+        "fontFamily",
+        "|",
+        "fontColor",
+        "fontBackgroundColor",
+        "|",
+        "bold",
+        "italic",
+        "underline",
+        "strikethrough",
+        "subscript",
+        "superscript",
+        "code",
+        "|",
+        "alignment",
+        "|",
+        "numberedList",
+        "bulletedList",
+        "|",
+        "outdent",
+        "indent",
+        "|",
+        "todoList",
+        "link",
+        "blockQuote",
+        "horizontalLine",
+        "imageUpload",
+        "insertTable",
+        "mediaEmbed",
+        "specialCharacters",
+      ],
+    },
+    language: "en",
+    image: {
+      toolbar: [
+        "imageTextAlternative",
+        "toggleImageCaption",
+        "imageStyle:inline",
+        "imageStyle:block",
+        "imageStyle:side",
+        "linkImage",
+      ],
+    },
+    table: {
+      contentToolbar: [
+        "tableColumn",
+        "tableRow",
+        "mergeTableCells",
+        "tableCellProperties",
+        "tableProperties",
+      ],
+    },
+  };
+}
